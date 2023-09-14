d4416d6514cd34628a4a8a262da6426a158f17dd
Farukcan Uğurlu <101269959+farukcanugurlu@users.noreply.github.com>
2023-09-14 22:07:34 +0300

    Create headsOrTails.js

---
+++ basicfunctions/headsOrTails.js
@@ -0,0 +1,13 @@
+// Basic Heads or Tails App
+
+function headsOrTails(){
+   let random=Math.random();
+   if(random>0.5){
+          console.log("Heads");
+   }
+   else{
+          console.log("Tails")
+   }
+}
+
+headsOrTails();
