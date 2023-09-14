1ba71859f588b20e19939d79f464eb8af04f08d5
Farukcan Uğurlu <101269959+farukcanugurlu@users.noreply.github.com>
2023-09-14 22:07:04 +0300

    Basic Print Function Writes Word As Many As You Want

---
+++ basicfunctions/print.js
@@ -0,0 +1,10 @@
+// Basic Print Function Writes Word As Many As You Want
+
+function howManyPrint(word,times){
+          for(let i=0;i<times;i++)
+          {
+                    console.log(word);
+          }
+}
+
+howManyPrint("faruk",25);
