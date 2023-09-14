61b1a6eb5ac81e115e9c5e6783d5c778b9a3df41
Farukcan Uğurlu <101269959+farukcanugurlu@users.noreply.github.com>
2023-09-14 22:06:08 +0300

    Calculate Area and Perimeter

---
+++ basicfunctions/areaPerimeter.js
@@ -0,0 +1,9 @@
+// Calculate Area and Perimeter
+
+function areaPerimeter(long,short){
+          let area=long*short;
+          let perimeter=(long+short)*2;
+          console.log(`Area:${area} Perimeter:${perimeter}`)
+}
+
+areaPerimeter(5,5);
